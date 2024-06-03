b72bfe6e4d02da9420056515d60bf71966491537
Shivam Agarwal <shivamagarwal5577@gmail.com>
2024-06-03 23:41:11 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'home_page.dart';
+
+void main() async {
+  // init the hive
+  await Hive.initFlutter();
+
+  // open a box
+  var box = await Hive.openBox('mybox');
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+      theme: ThemeData(primarySwatch: Colors.yellow),
+    );
+  }
+}
+// Primary Color: Soft Blue (#4A90E2)
+// Accent Color: Coral (#FF6B6B)
+// Background Color: Light Grey (#F7F7F7)
+// Card Color: White (#FFFFFF)
+// Text Color: Dark Grey (#4A4A4A)
+// Secondary Text Color: Medium Grey (#9B9B9B)
+// Button Color: Soft Green (#7ED321)
